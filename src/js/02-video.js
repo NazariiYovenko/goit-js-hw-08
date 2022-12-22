cad6adc8880bee16134442f834ad94121905134c
NazariiYovenko <nazarjovenko@gmail.com>
2022-12-22 18:03:40 +0200

    02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const savedData = localStorage.getItem('player-time');
+
+if (savedData) {
+  player.setCurrentTime(savedData);
+}
+
+player.on('timeupdate', throttle(saveTimeData, 1000));
+function saveTimeData(data) {
+  localStorage.setItem('player-time', data.seconds);
+}
